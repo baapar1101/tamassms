9859fac7ca734133663a8ff183c75c43f8b3163d
Albert Heidarian <50908145+baapar1101@users.noreply.github.com>
2023-11-09 01:09:50 +0330

    Add files via upload

---
+++ server.js
@@ -0,0 +1,74 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const url = require('url');
+
+const server = http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+  let pathname = `.${parsedUrl.pathname}`;
+
+  if (req.method === 'POST' && req.url === '/send-sms') {
+    let body = '';
+    req.on('data', chunk => {
+      body += chunk.toString();
+    });
+
+    req.on('end', () => {
+      try {
+        const data = JSON.parse(body);
+        // Simulate sending SMS
+        console.log('SMS Data:', data);
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ message: 'SMS sent successfully' }));
+      } catch (error) {
+        res.writeHead(400, { 'Content-Type': 'text/plain' });
+        res.end('Error in JSON data');
+      }
+    });
+  } else {
+    if (pathname === './') {
+      pathname = './index.html'; // Set default file to serve
+    }
+
+    const ext = path.parse(pathname).ext;
+    let contentType = 'text/html';
+
+    switch (ext) {
+      case '.js':
+        contentType = 'text/javascript';
+        break;
+      case '.css':
+        contentType = 'text/css';
+        break;
+      case '.json':
+        contentType = 'application/json';
+        break;
+      case '.png':
+        contentType = 'image/png';
+        break;
+      case '.jpg':
+        contentType = 'image/jpg';
+        break;
+    }
+
+    fs.readFile(pathname, (err, data) => {
+      if (err) {
+        if (err.code === 'ENOENT') {
+          res.writeHead(404);
+          res.end('404 Not Found');
+        } else {
+          res.writeHead(500);
+          res.end(`Server Error: ${err.code}`);
+        }
+      } else {
+        res.writeHead(200, { 'Content-Type': contentType });
+        res.end(data, 'utf-8');
+      }
+    });
+  }
+});
+
+const port = 8000; // Set the port you want the server to listen on
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
