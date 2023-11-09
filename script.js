1b735903eb86038381d7ac0dbd447091f23bfdbd
Albert Heidarian <50908145+baapar1101@users.noreply.github.com>
2023-11-09 15:37:28 +0330

    test

--- script.js
+++ script.js
@@ -1,40 +1,23 @@
-document.getElementById('sendButton').addEventListener('click', function() {
-    var apiKey = document.getElementById('apiKeyInput').value;
-    var messageText = document.getElementById('messageInput').value;
-    var sender = document.getElementById('senderInput').value;
-    var recipients = document.getElementById('recipientsInput').value.split(',');
+document.getElementById('apiForm').addEventListener('submit', function(event) {
+    event.preventDefault();
   
-    // Create an object to hold the request data
-    var requestData = {
-      apiKey: apiKey,
-      messageText: messageText,
-      sender: sender,
-      recipients: recipients
+    var apiKey = document.getElementById('apiKey').value;
+    var text = document.getElementById('text').value;
+    var sender = document.getElementById('sender').value;
+    var recipients = document.getElementById('recipients').value.split(',');
+  
+    var config = {
+      method: 'get',
+      url: `https://api.sms-webservice.com/api/V3/Send?ApiKey=${apiKey}&Text=${text}&Sender=${sender}&Recipients=${recipients.join(',')}`,
+      headers: {}
     };
   
-    // Convert the request data to JSON
-    var jsonData = JSON.stringify(requestData);
-  
-    // Send the API request
-    sendApiRequest(jsonData);
+    axios(config)
+      .then(function(response) {
+        document.getElementById('response').innerText = JSON.stringify(response.data);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
   });
   
-  function sendApiRequest(jsonData) {
-    // Perform the POST request to your API endpoint
-    fetch('your_api_endpoint', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: jsonData
-    })
-    .then(response => response.json())
-    .then(data => {
-      // Update the result element with the received data
-      document.getElementById('result').textContent = 'Result: ' + JSON.stringify(data);
-    })
-    .catch(error => {
-      // Handle any errors that occur during the request
-      console.error('Error:', error);
-    });
-  }
